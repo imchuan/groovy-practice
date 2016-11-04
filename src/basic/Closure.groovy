18d6351b2ddc66d958c992208558016182373c66
Leon <muchuanj@163.com>
2016-11-04 17:01:53 +0800

    add files

--- src/basic/Closure.groovy
+++ src/basic/Closure.groovy
@@ -14,5 +14,24 @@ cols1.call("Leon")
 cols1("Hello World")
 
 // 单个隐参数
-def cols2 = {println it}
+def cols2 = { println it }
 cols2("Hello cols2")
+
+// 访问变量
+def wel = "Hello"
+def say = { params -> println "${wel},${params}" }
+say("Leon")
+
+wel = "Welcome"
+say("Leon")
+
+
+[1, 2, 3, 4, 5].each { println it }
+def student = ["name": "Leon", "age": 25]
+student.each { println "${it.key} = ${it.value}" }
+
+def nums = [1, 2, 3, 4, 5]
+nums.each { num -> if (num == 2) println num }
+
+def result = nums.find({ it > 3 }) // 返回第一个符合条件的值
+println "result = $result"
