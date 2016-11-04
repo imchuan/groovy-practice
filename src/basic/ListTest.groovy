235e639f50ac61f192a9634a9ccd0964e4619912
Leon <muchuanj@163.com>
2016-11-04 10:58:41 +0800

    add files

--- src/basic/ListTest.groovy
+++ src/basic/ListTest.groovy
@@ -3,16 +3,33 @@ package basic
 /**
  * @author Leon
  */
-class ListTest {
-    static void main(args) {
-        def numbers = [1, 2, 3, 4, 5]
-        println(numbers[2])
-        println(numbers << 6)
+// 定义一个整数型列表
+def numbers = [1, 2, 22, 44]
+assert numbers[0] == 1 // 断言第一个元素是否等于1
+assert numbers[2] == 22
+// 索引可以是负数,表示从列表的结尾往前数
+assert numbers[-2] == 22
 
-        def result = ["name":"liuqq","age":12]
-        println(result.name)
+// 可以使用范围(start..end)操作列表，返回一个新的列表
+println numbers[0..2] // 返回 [1, 2, 22]，包括end边界
+println numbers[0..<2] // 返回 [1, 2]，不包括end边界
 
-        def str = 1..9
-        println(str)
-    }
-}
+// 赋值
+numbers[0] = 3
+println numbers
+
+numbers[0] = [3, 2]
+println numbers
+
+// 将元素追加到列表的最后
+numbers << 33
+println numbers
+
+// 链接两个列表
+def n = [1, 2, 3]
+def b = n + [55, 66]
+println b
+
+// 删除一个元素
+def c = b - [66]
+println c
---
+++ src/basic/Mapping.groovy
@@ -0,0 +1,13 @@
+package basic
+
+/**
+ * 映射
+ * @author Leon
+ */
+
+def student = ["name":"Leon", "age": 25]
+println student.name
+println student["name"]
+
+student["name"] = "Li"
+println student
